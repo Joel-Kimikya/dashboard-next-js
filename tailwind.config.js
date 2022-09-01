900ae195db1852e0a53931ca9d053ece587b2f59
joelcena <joelcena.47@gmail.com>
2022-08-31 17:59:07 -0700

    we changed our font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blck: '#FAFAFA',
+      },
+    },
+    fontFamily: {
+      mulish: ['Mulish', 'sans-serif'],
+    },
   },
   plugins: [],
 };
